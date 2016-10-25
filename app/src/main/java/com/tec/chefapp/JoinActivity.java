d422541b267798e10b2e8f5ae92379c30e3ae2b7
AaronSolera <aaron.smartinez1997@gmail.com>
2016-10-25 17:29:55 -0600

    LinkedIn
    Integración con LinkedIn completada

--- app/src/main/java/com/tec/chefapp/JoinActivity.java
+++ app/src/main/java/com/tec/chefapp/JoinActivity.java
@@ -1,13 +1,84 @@
 package com.tec.chefapp;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.media.Image;
+import android.os.StrictMode;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.linkedin.platform.LISessionManager;
+import com.linkedin.platform.errors.LIAuthError;
+import com.linkedin.platform.listeners.AuthListener;
+import com.linkedin.platform.utils.Scope;
 
 public class JoinActivity extends AppCompatActivity {
 
+    ImageButton join;
+    //Esto es un método que me permite pasar a la siguiente activity si ingreso con mi cuenta de LinkedIn
+    public void nextActivity(ImageButton button, final Activity previous, final Class next)
+    {
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                login_linkedin();
+            }
+        });
+    }
+    //Método principal de la actividad, aquí se carga todo
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_join);
+        join= (ImageButton) findViewById(R.id.linkedin_button);
+        nextActivity(join,this,MainActivity.class);
+    }
+    // Método que me permite validar si la autentificación de la cuenta de LinkedIn fue exitosa, de lo contrario muestra el código del error
+    public void login_linkedin(){
+        LISessionManager.getInstance(getApplicationContext()).init(this, buildScope(), new AuthListener() {
+            @Override
+            public void onAuthSuccess() {
+
+            }
+
+            @Override
+            public void onAuthError(LIAuthError error) {
+                Toast.makeText(getApplicationContext(), "Failed :C" + error.toString(), Toast.LENGTH_LONG).show();
+            }
+        },true);
+    }
+    //Este método me permite transferir los datos obtenidos de LinkedIn para ser usados en otra actividad, cuando nota la acción del método login_linkedin, ella cambia la actividad actual por la que se estipuló
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        LISessionManager.getInstance(getApplicationContext()).onActivityResult(this, requestCode, resultCode, data);
+        Intent intent=new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+    //Me permite obtener información del perfil del usuario, sustrayéndola de su cuenta de LinkedIn
+    private static Scope buildScope(){
+        return Scope.build(Scope.R_BASICPROFILE,Scope.R_EMAILADDRESS);
+    }
+
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == event.KEYCODE_BACK) {
+            finish();
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.addCategory(Intent.CATEGORY_HOME);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
---
+++ app/src/main/java/com/tec/chefapp/MainActivity.java
@@ -0,0 +1,176 @@
+package com.tec.chefapp;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.linkedin.platform.APIHelper;
+import com.linkedin.platform.LISessionManager;
+import com.linkedin.platform.errors.LIApiError;
+import com.linkedin.platform.listeners.ApiListener;
+import com.linkedin.platform.listeners.ApiResponse;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private static final String topCardUrl = "https://api.linkedin.com/v1/people/~:(email-address,formatted-name,phone-numbers,public-profile-url,picture-url,picture-urls::(original))";
+    ProgressDialog progress;
+    ImageView profile_pic;
+    TextView user_name, user_email;
+    NavigationView navigation_view;
+    Button logout;
+    //Método principal en donde se define todo
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        logout = (Button) findViewById(R.id.logout);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Ordenes");
+        progress = new ProgressDialog(this);
+        progress.setMessage("Retrieve data...");
+        progress.setCanceledOnTouchOutside(false);
+        progress.show();
+        getUserData(); //Método definido por mi
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+        setNavigationHeader(); //Método definido por mi
+        navigation_view.setNavigationItemSelectedListener(this);
+        //maneja la acción del botón logout
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LISessionManager.getInstance(getApplicationContext()).clearSession();
+                Intent intent = new Intent(MainActivity.this, JoinActivity.class);
+                finish();
+                startActivity(intent);
+            }
+        });
+    }
+    //La información del ususario es obtenida mediante paquetes JSON enviados por el servidor de LinkedIn
+    public void getUserData()
+    {
+        APIHelper apiHelper = APIHelper.getInstance(getApplicationContext());
+        apiHelper.getRequest(MainActivity.this, topCardUrl, new ApiListener() {
+            @Override
+            public void onApiSuccess(ApiResponse apiResponse) {
+                try {
+                    setUserProfile(apiResponse.getResponseDataAsJson());
+                    progress.dismiss();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onApiError(LIApiError LIApiError) {
+
+            }
+        });
+    }
+    //Añade a la barra de acción, el nombre, email y foto del usuario de LinkedIn
+    public void  setNavigationHeader(){
+        navigation_view = (NavigationView) findViewById(R.id.nav_view);
+
+        View header = LayoutInflater.from(this).inflate(R.layout.nav_header_main,null);
+        navigation_view.addHeaderView(header);
+
+        user_name = (TextView) header.findViewById(R.id.username);
+        user_email = (TextView) header.findViewById(R.id.email);
+        profile_pic = (ImageView) header.findViewById(R.id.profile_pic);
+    }
+    //Asigna los datos a mostrar, oteniendolos de las respuestas del servidor de LinkedIn, mediante un JSON
+    public void setUserProfile(JSONObject response){
+        try{
+            user_email.setText(response.get("emailAddress").toString());
+            user_name.setText(response.get("formattedName").toString());
+            Picasso.with(this).load(response.getString("pictureUrl")).into(profile_pic);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+    //Añade items a la barra de acción
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    //Maneja la acción del botón que muestra la barra de acción, además la esconde si se presiona la tecla back
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    //Maneja las acciones presentes en la barra de acción
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == event.KEYCODE_BACK) {
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
